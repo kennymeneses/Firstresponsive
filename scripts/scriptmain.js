ac736fedb56549e7b930b2242718a12b24f7d1fa
Kenny Meneses <kennny16m@gmail.com>
2020-03-28 18:27:37 -0500

    Panel Responsive con dimensiones correctas

--- scripts/scriptmain.js
+++ scripts/scriptmain.js
@@ -7,8 +7,6 @@ hamburguesa.addEventListener('click',()=>{
         if(nav.style.right != "0px")
         {
             nav.style.right = "0"
-            console.log("funciona")
-            console.log(nav.style.right)
         }
         else{
             nav.style.right = "-262.5px"
