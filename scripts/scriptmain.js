768a45b939c4e7a648da6e26787506cb7ee86423
Kenny Meneses <kennny16m@gmail.com>
2020-03-28 17:30:58 -0500

    El panel derecho se muestra y se oculta

--- scripts/scriptmain.js
+++ scripts/scriptmain.js
@@ -1,1 +1,18 @@
-console.log('lalalalala')
+const nav = document.getElementById('main-nav')
+const hamburguesa = document.getElementById('main-menu-toogle')
+
+
+hamburguesa.addEventListener('click',()=>{
+
+        if(nav.style.right != "0px")
+        {
+            nav.style.right = "0"
+            console.log("funciona")
+            console.log(nav.style.right)
+        }
+        else{
+            nav.style.right = "-262.5px"
+        }
+
+    }
+)
